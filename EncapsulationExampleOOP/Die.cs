3e1cc32c5cec3868715e694ba0cc578e33cb424d
Mary Saray <msaray.ms@outlook.com>
2021-07-15 18:56:17 -0700

    Created a class to represent and individual dice.

---
+++ EncapsulationExampleOOP/Die.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExampleOOP
+{
+    /// <summary>
+    /// Represents an individual dice.
+    /// </summary>
+    class Die
+    {
+    }
+}
