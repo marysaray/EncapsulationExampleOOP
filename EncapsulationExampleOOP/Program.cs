38fbbe323f73d5321e865794af12af72ddf0d048
Mary Saray <msaray.ms@outlook.com>
2021-07-15 21:08:59 -0700

    Implemented the die class to generate random values.

--- EncapsulationExampleOOP/Program.cs
+++ EncapsulationExampleOOP/Program.cs
@@ -10,6 +10,20 @@ namespace EncapsulationExampleOOP
     {
         static void Main(string[] args)
         {
+            // create an array of 5 individual die to reference
+            Die[] dice = new Die[5]; // each element in the array is a Die object.
+
+            // create a loop to go through each one.
+            for (int i = 0; i < dice.Length; i++)
+            {
+                // create the die, call the constructor to roll the die
+                dice[i] = new Die(); // set to avoid default settings of 0 || null.
+
+                // display the values
+                Console.WriteLine(dice[i].Value);
+            }
+
+            Console.ReadKey(); // Keeps program open.
         }
     }
 }
